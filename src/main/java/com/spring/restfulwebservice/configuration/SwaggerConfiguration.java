c4ab3be2ba7939c6d30cd6556260b02874cc0ef7
gauravpathak <gauravpathak.spring@gmail.com>
2018-12-30 16:00:38 +0530

    Content negotiation for XML. Closes #6

--- src/main/java/com/spring/restfulwebservice/configuration/SwaggerConfiguration.java
+++ src/main/java/com/spring/restfulwebservice/configuration/SwaggerConfiguration.java
@@ -2,16 +2,25 @@ package com.spring.restfulwebservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+    public static final Contact DEFAULT_CONTACT = new Contact("", "", "");
+    public static final ApiInfo DEFAULT = new ApiInfo("Api Documentation", "Gaurav USERS API Documentation",
+            "1.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList());
+
     @Bean
     public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT);
     }
 }
